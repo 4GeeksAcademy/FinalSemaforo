888db185c67a68b473b0f14d775214834fe996a0
Julitoo01 <jlapujun@gmail.com>
2026-03-13 09:33:46 +0000

    FinalSemaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [luzActiva, setLuzActiva] = useState("rojo");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setLuzActiva((colorActual) => {
+        if (colorActual === "rojo") return "verde";
+        if (colorActual === "verde") return "amarillo";
+        if (colorActual === "amarillo") return "rojo";
+      });
+    }, 3000);
+
+    return () => clearInterval(intervalo);
+  }, []);
+
+  return (
+    <div className="container text-center mt-5">
+      <h1>Semáforo Bootstrap</h1>
+
+      <div
+        className="semaforo bg-dark p-4 mx-auto rounded d-flex flex-column align-items-center"
+        style={{ width: "100px" }}
+      >
+        <button
+          className={`btn rounded-circle mb-3 ${
+            luzActiva === "rojo" ? "bg-danger" : "bg-secondary"
+          }`}
+          style={{ width: "60px", height: "60px" }}
+        ></button>
+
+        <button
+          className={`btn rounded-circle mb-3 ${
+            luzActiva === "amarillo" ? "bg-warning" : "bg-secondary"
+          }`}
+          style={{ width: "60px", height: "60px" }}
+        ></button>
+
+        <button
+          className={`btn rounded-circle ${
+            luzActiva === "verde" ? "bg-success" : "bg-secondary"
+          }`}
+          style={{ width: "60px", height: "60px" }}
+        ></button>
+      </div>
+
+      <style>{`
+        .bg-secondary {
+          opacity: 0.3;
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default Home;
